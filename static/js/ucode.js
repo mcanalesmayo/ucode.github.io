b189ad2d68712ea35d4d864a788281cfc5e52393
jorcox <jorgecancerps@gmail.com>
2017-10-29 19:47:25 +0100

    Testing III

--- static/js/ucode.js
+++ static/js/ucode.js
@@ -4,5 +4,6 @@ img = document.getElementById('screw');
 $(document).ready(setInterval(function() {
   img = document.getElementById('screw');
   angle = (angle + 1) % 360;
-  img.style = "transform:rotate(" + angle + ");";
+  img.css("background-color", "yellow");
+  img.style = "transform:rotate(" + angle + "deg);";
 }, 300));
