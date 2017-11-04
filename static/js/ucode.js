6b49146e3a47b6192dd31c90b1b2d7965bdcb62c
jorcox <jorgecancerps@gmail.com>
2017-11-04 22:17:50 +0100

    Adding styles

--- static/js/ucode.js
+++ static/js/ucode.js
@@ -1,8 +1,12 @@
 var angle = 0,
 img = document.getElementById('screw');
 
-$(document).ready(setInterval(function() {
-  img = document.getElementById('screw');
-  angle = (angle + 1) % 360;
-  img.style = "transform:rotate(" + angle + "deg); display:block; margin:auto; max-width:60%; max-height:60%;";
-}, 30));
+$(document).ready(
+  setInterval(
+    function() {
+      img = document.getElementById('screw');
+      angle = (angle + 1) % 360;
+      img.style = "transform:rotate(" + angle + "deg); display:block; margin:auto; max-width:60%; max-height:60%;";
+  }, 30)
+
+);
