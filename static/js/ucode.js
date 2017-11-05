81fc2fc858508c946cdfd47f86de7bc219d77f07
jorcox <jorgecancerps@gmail.com>
2017-11-05 12:23:02 +0100

    Adding button

--- static/js/ucode.js
+++ static/js/ucode.js
@@ -6,7 +6,7 @@ $(document).ready(
     function() {
       img = document.getElementById('screw');
       angle = (angle + 1) % 360;
-      img.style = "transform:rotate(" + angle + "deg); display:block; margin:auto; max-width:30%; max-height:30%; margin-top:5% margin-bottom:5%";
+      img.style = "transform:rotate(" + angle + "deg); display:block; margin:auto; max-width:30%; max-height:30%; margin-top:5%; margin-bottom:5%;";
   }, 30)
 
 );
